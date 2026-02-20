c818e34e6c3591be8d3b0c2063d6f90a3721fd5e
QuZee97 <cador92@googlemail.com>
2026-02-20 17:48:33 +0100

    fix: CSV batch 15, ELSTER XML export, checkbox filter bar

--- api/tax.js
+++ api/tax.js
@@ -204,7 +204,7 @@ module.exports = async function handler(req, res) {
         ],
         response_format: { type: 'json_object' },
         temperature: 0.1,
-        max_tokens: 4000,
+        max_tokens: 2000,
       }),
     });
 
